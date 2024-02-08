47c73a82a6804210bf0b29c4d25fbd698262bde0
annaromaniuk11 <143599695+annaromaniuk11@users.noreply.github.com>
2024-02-08 19:32:34 +0000

    Deploying to gh-pages from @ annaromaniuk11/goit-js-hw-10@8cbb5f47029cde3774da352fb1e48c05452610fb 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const s=document.querySelector(".form");s.addEventListener("submit",function(o){o.preventDefault();const t=parseInt(s.elements.delay.value,10),m=s.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{m==="fulfilled"?e(t):r(t)},t)}).then(e=>{i.success({title:"Fulfilled promise",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Rejected promise",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
